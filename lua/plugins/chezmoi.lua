eaa5168f66710d9d56486dfb7340d6057ea38a55
Brendan Lees <brendan@steadydigital.co>
2025-07-17 21:56:49 +1000

    removed unneeded

--- lua/plugins/chezmoi.lua
+++ lua/plugins/chezmoi.lua
@@ -5,7 +5,7 @@ return {
     init = function()
       -- This option is required.
       vim.g["chezmoi#use_tmp_buffer"] = true
-      vim.g["chezmoi#use_external"] = 1
+      -- vim.g["chezmoi#use_external"] = 1
     end,
   },
 }
