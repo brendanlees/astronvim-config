e2450c21d1324d884151f760d4885eedad0d5261
Brendan Lees <brendan@steadydigital.co>
2025-11-25 06:05:50 +1000

    add scroll, pack options

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,14 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.chezmoi" },
+  { import = "astrocommunity.pack.docker" },
   { import = "astrocommunity.colorscheme.tokyonight-nvim" },
   { import = "astrocommunity.recipes.cache-colorscheme" },
+  { import = "astrocommunity.scrolling.mini-animate" },
+  { import = "astrocommunity.scrolling.vim-smoothie" },
+  { import = "astrocommunity.scrolling.cinnamon.nvim" },
+  { import = "astrocommunity.indent.indent-rainbowline" },
 }
