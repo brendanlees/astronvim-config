88dcb52a18654706735c744cc4d247105657ed68
Brendan Lees <brendan@steadydigital.co>
2026-04-20 15:22:08 +1000

    fix(treesitter): replace ensure_installed with auto_install
    ensure_installed races with community pack parser installations,
    causing "mkdir: File exists" errors on startup. auto_install
    compiles parsers lazily when the filetype is opened, avoiding
    the race entirely.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,46 +4,12 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
+    auto_install = true,
     highlight = {
       disable = function()
         -- check if 'filetype' option includes 'chezmoitmpl'
         if string.find(vim.bo.filetype, "chezmoitmpl") then return true end
       end,
-    },
-    ensure_installed = {
-      "lua",
-      "vim",
-      "gotmpl",
-      "toml",
-      "yaml",
-      "blade",
-      "css",
-      "dockerfile",
-      "gitignore",
-      "git_config",
-      "go",
-      "graphql",
-      "html",
-      "http",
-      "javascript",
-      "jinja",
-      "json",
-      "nginx",
-      "nix",
-      "php",
-      "python",
-      "regex",
-      "rust",
-      "scss",
-      "sql",
-      "ssh_config",
-      "terraform",
-      "tmux",
-      "twig",
-      "typescript",
-      "xml",
-      "zig",
-      "zsh",
     },
   },
 }
