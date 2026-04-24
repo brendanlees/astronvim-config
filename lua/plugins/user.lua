b1d950a658128e1d6b78931782a77e161519f3ea
Brendan Lees <brendan@steadydigital.co>
2026-04-25 09:35:42 +1000

    feat(plugins): disable which-key popup

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -2,11 +2,6 @@
 return {
 
   "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
 
   -- == neo-tree ==
   {
@@ -50,6 +45,7 @@ return {
 
   -- You can disable default plugins as follows:
   { "max397574/better-escape.nvim", enabled = true },
+  { "folke/which-key.nvim", enabled = false },
 
   -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
   {
