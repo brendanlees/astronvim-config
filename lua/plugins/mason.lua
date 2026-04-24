4fb42b34685b73690b19fbbb766f86200a73f7f3
Brendan Lees <brendan@steadydigital.co>
2026-04-25 09:41:29 +1000

    fix(mason): use correct package names and document tool categories

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,13 +2,25 @@
 
 ---@type LazySpec
 return {
+  -- use mason-tool-installer for automatically installing Mason packages
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
+    -- overrides `require("mason-tool-installer").setup(...)`
     opts = {
+      -- Make sure to use the names found in `:Mason`
       ensure_installed = {
-        "systemd-lsp",
+        -- install language servers
+        "systemd-language-server",
         "astro-language-server",
+
+        -- install formatters
         "stylua",
+
+        -- install debuggers
+        -- "debugpy",
+
+        -- install any other package
+        "tree-sitter-cli",
       },
     },
   },
