3a0a96a501c6433473ea0a74a735ce06ed8ffe98
Brendan Lees <brendan@steadydigital.co>
2026-04-25 09:41:33 +1000

    fix(treesitter): replace auto_install with explicit ensure_installed list

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,12 +4,47 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    auto_install = true,
     highlight = {
       disable = function()
         -- check if 'filetype' option includes 'chezmoitmpl'
         if string.find(vim.bo.filetype, "chezmoitmpl") then return true end
       end,
+    },
+    ensure_installed = {
+      "lua",
+      "vim",
+      "gotmpl",
+      "toml",
+      "yaml",
+      "blade",
+      "css",
+      "dockerfile",
+      "gitignore",
+      "git_config",
+      "go",
+      "graphql",
+      "html",
+      "http",
+      "javascript",
+      -- "jinja2", not available?
+      "json",
+      "nginx",
+      "nix",
+      "php",
+      "python",
+      "regex",
+      "rust",
+      "scss",
+      "sql",
+      "ssh_config",
+      -- "ssh_client_config", not available?
+      "terraform",
+      "tmux",
+      "twig",
+      "typescript",
+      "xml",
+      "zig",
+      -- "zsh", not available?
     },
   },
 }
